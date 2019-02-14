03764bbfbe81a2ab41711d66ebce2af5ce22ac86
RCJacH <RCJacH@outlook.com>
2019-02-14 11:52:00 +0800

    + Command line argument parser

--- monolizer/__init__.py
+++ monolizer/__init__.py
@@ -2,5 +2,6 @@ __author__ = 'RCJacH'
 __email__ = 'RCJacH@outlook.com'
 __version__ = '0.1.0'
 
+
 from .monolizer import Monolizer
 from .file_handler import FileHandler
---
+++ monolizer/__main__.py
@@ -0,0 +1,70 @@
+from __future__ import print_function
+
+import sys
+import os
+
+
+def parser(args):
+    from argparse import ArgumentParser
+
+    parser = ArgumentParser(description='Convert all fake stereo audio files under current directory into mono channel files, with the option to delete empty audio files.')
+    parser.add_argument('-a', '--auto',
+                        action='store_true',
+                        help='Backup to "RAW", \
+                        delete empty audio files, \
+                        and monolize fake stereos.')
+    parser.add_argument('-b', '--backup',
+                        nargs=1, default=['RAW'],
+                        help='Name of the sub-folder to backup all files to. \
+                        Default: "RAW"')
+    parser.add_argument('-i', '--inc',
+                        action='store_true',
+                        help='Add a number to the backup folder name if the \
+                        folder exists to avoid overwriting existing folder.')
+    # parser.add_argument('-f', '--folder',
+    #                     nargs=1, default='',
+    #                     help='Output folder for monolized files.')
+    parser.add_argument('-d', '--delete',
+                        action='store_true',
+                        help='Delete all empty/silent audio files')
+    parser.add_argument('-m', '--monolize',
+                        action='store_true',
+                        help='Convert all fake stereo files into mono files.')
+    parser.add_argument('-o', '--overwrite',
+                        action='store_true',
+                        help='Make permanent change to the original file \
+                        without backing up.')
+    # parser.add_argument('-s', '--subfolder',
+    #                     action='store_true',
+    #                     help='Include files in sub-folder during conversion.')
+    args = parser.parse_args()
+    return args
+
+
+def main(args=None):
+    if args is None:
+        args = sys.argv[1:]
+    args = parser(args)
+
+    from monolizer import FileHandler
+
+    folder = FileHandler(folder=os.getcwd())
+    if len(folder.files) == 0:
+        print('No audio files found in current directory.')
+    else:
+        if args.auto or args.delete or args.monolize:
+            if args.auto or (not args.overwrite and args.backup):
+                folder.backup(folder=args.backup[0], newfolder=args.inc)
+                print('Backed up all original files to {} subfolder'.format(args.backup[0]))
+            if args.auto or args.delete:
+                files = folder.delete_empty_files()
+                print('Deleted files: {}'.format(' '.join(files)))
+            if args.auto or args.monolize:
+                files = folder.monolize_fake_stereo_files()
+                print('Monolized files: {}'.format(' '.join(files)))
+        else:
+            print(folder)
+
+
+if __name__ == "__main__":
+    main()
